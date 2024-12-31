a027432a92889a44886f21114f00982533285f78
Kaan Faruk Kınalı <62002518+KFKMan@users.noreply.github.com>
2024-12-31 21:45:23 +0300

    Fixing fake present, changing WriteLine to Write in the getting input, adding exitable while loop

--- WebsiteConsoleChecker/Program.cs
+++ WebsiteConsoleChecker/Program.cs
@@ -10,7 +10,7 @@ void PrintLineWithColor(string text, ConsoleColor color)
 
 string GetInput(string question)
 {
-	Console.WriteLine(question);
+	Console.Write(question);
 	return Console.ReadLine() ?? GetInput(question);
 }
 
@@ -26,63 +26,72 @@ void PrintErrorState()
 	Console.WriteLine($"ForOutputRegexing-Error-Accoured-State:{errorAccoured}:");
 }
 
-var target = GetInput("Please Enter Target Website Url: ");
-
-Console.WriteLine("Downloading Required Files");
-var browserFetcher = new BrowserFetcher();
-await browserFetcher.DownloadAsync(); //Download Required Files
-Console.WriteLine("Required Files Downloaded");
-
-await using (var browser = await Puppeteer.LaunchAsync(
-	new LaunchOptions { Headless = true })) //Launch browser
+while (true)
 {
-	await using (var page = await browser.NewPageAsync()) //Create new page in browser
+	var target = GetInput("Please Enter Target Website Url: ");
+
+	Console.WriteLine("Downloading Required Files");
+	var browserFetcher = new BrowserFetcher();
+	await browserFetcher.DownloadAsync(); //Download Required Files
+	Console.WriteLine("Required Files Downloaded");
+
+	await using (var browser = await Puppeteer.LaunchAsync(
+		new LaunchOptions { Headless = true })) //Launch browser
 	{
-		page.Error += (sender, e) =>
+		await using (var page = await browser.NewPageAsync()) //Create new page in browser
 		{
-			ErrorAccoured();
-			PrintLineWithColor($"Internal System Error => {e.Error}", ConsoleColor.Red);
-		};
-
-		page.PageError += (sender, e) =>
-		{
-			ErrorAccoured();
-			PrintLineWithColor($"Page Error => {e.Message}", ConsoleColor.Red);
-		};
-
-		page.Console += (sender, e) =>
-		{
-			if (e.Message.Type == ConsoleType.Error)
+			page.Error += (sender, e) =>
 			{
 				ErrorAccoured();
-				PrintLineWithColor($"Console Error => {e.Message.Text}", ConsoleColor.Red);
-			}
-		};
+				PrintLineWithColor($"Internal System Error => {e.Error}", ConsoleColor.Red);
+			};
 
-		Console.WriteLine("Going to Target Website");
-		var rep = await page.GoToAsync(target, new NavigationOptions()
-		{
-			Timeout = 60,
-			WaitUntil = new[]{
+			page.PageError += (sender, e) =>
+			{
+				ErrorAccoured();
+				PrintLineWithColor($"Page Error => {e.Message}", ConsoleColor.Red);
+			};
+
+			page.Console += (sender, e) =>
+			{
+				if (e.Message.Type == ConsoleType.Error)
+				{
+					ErrorAccoured();
+					PrintLineWithColor($"Console Error => {e.Message.Text}", ConsoleColor.Red);
+				}
+			};
+
+			Console.WriteLine("Going to Target Website");
+			var rep = await page.GoToAsync(target, new NavigationOptions()
+			{
+				Timeout = TimeSpan.FromSeconds(60).Milliseconds,
+				WaitUntil = new[]{
 				WaitUntilNavigation.Networkidle0
 			}
-		});
+			});
 
-		if(rep.Status != System.Net.HttpStatusCode.OK)
-		{
-			Console.WriteLine("Status Code is not 200 (OK), maybe a problem?");
-			return;
-		}
+			if (rep.Status != System.Net.HttpStatusCode.OK)
+			{
+				Console.WriteLine("Status Code is not 200 (OK), maybe a problem?");
+				return;
+			}
 
-		if (errorAccoured)
-		{
-			PrintLineWithColor("No Error Accoured", ConsoleColor.Green);
-		}
-		else
-		{
-			PrintLineWithColor("Error Accoured", ConsoleColor.Red);
-		}
+			if (!errorAccoured)
+			{
+				PrintLineWithColor("No Error Accoured", ConsoleColor.Green);
+			}
+			else
+			{
+				PrintLineWithColor("Error Accoured", ConsoleColor.Red);
+			}
 
-		PrintErrorState();
+			PrintErrorState();
+		}
+	}
+
+	var res = GetInput("Do you want to exit (0) or continue (any other value)?");
+	if(res.Trim() == "0")
+	{
+		break;
 	}
 }
